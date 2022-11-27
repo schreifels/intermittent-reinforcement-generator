7850925248e5c389b5aa4d149c3ff3ffc14d7795
Mike Schreifels <mike@schreifels.org>
2022-11-27 11:50:22 -0800

    Rename 'ratio' to 'percentage'

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const LOADING_DELAY_MS = 1050;
-const DEFAULT_RATIO = 85;
+const DEFAULT_PERCENTAGE = 85;
 const YES_CLASSNAME = "yes";
 const NO_CLASSNAME = "no";
 
-function getRatio() {
+function getPercentage() {
   const queryString = new URLSearchParams(window.location.search);
-  const ratio = queryString.get("ratio");
-  if (!ratio) {
+  const percentage = queryString.get("pct");
+  if (!percentage) {
     return;
   }
 
-  const parsedRatio = parseInt(ratio);
-  if (Number.isNaN(parsedRatio)) {
+  const parsedPercentage = parseInt(percentage);
+  if (Number.isNaN(parsedPercentage)) {
     return;
   }
 
-  return parsedRatio;
+  return parsedPercentage;
 }
 
-function redirectWithRatio() {
+function redirectWithPercentage() {
   const queryString = new URLSearchParams(window.location.search);
-  queryString.set("ratio", DEFAULT_RATIO);
+  queryString.set("pct", DEFAULT_PERCENTAGE);
   window.location.replace(window.location.pathname + "?" + queryString);
 }
 
@@ -28,18 +28,18 @@ function getRandomNumber1To100() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-function unhideElement(ratio, randomNumber) {
-  const className = randomNumber > ratio ? NO_CLASSNAME : YES_CLASSNAME;
+function unhideElement(percentage, randomNumber) {
+  const className = randomNumber > percentage ? NO_CLASSNAME : YES_CLASSNAME;
   document.body.classList.add(className);
 }
 
 window.addEventListener("load", () => {
-  const ratio = getRatio();
-  if (ratio === undefined) {
-    redirectWithRatio();
+  const percentage = getPercentage();
+  if (percentage === undefined) {
+    redirectWithPercentage();
     return;
   }
 
   const randomNumber = getRandomNumber1To100();
-  setTimeout(() => unhideElement(ratio, randomNumber), LOADING_DELAY_MS);
+  setTimeout(() => unhideElement(percentage, randomNumber), LOADING_DELAY_MS);
 });
